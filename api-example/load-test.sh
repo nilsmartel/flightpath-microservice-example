5ea7f9b3f226b5ebd8937aaede086d2966b1b700
Nils Martel <nilsmartel@yahoo.de>
2022-10-07 12:46:06 +0200

    Fix load test

--- api-example/load-test.sh
+++ api-example/load-test.sh
@@ -1,6 +1,6 @@
-for i in $(seq(10000))
+for i in $(seq 10000 )
 do
-curl -X GET localhost:8080/calculate \
-    -H "Content-Type: application/json" \
-    -d '[["IND", "EWR"], ["SFO", "ATL"], ["GSO", "IND"], ["ATL", "GSO"]]' &
-done
+    curl -X GET localhost:8080/calculate \
+        -H "Content-Type: application/json" \
+        -d '[["IND", "EWR"], ["SFO", "ATL"], ["GSO", "IND"], ["ATL", "GSO"]]' &
+    done
